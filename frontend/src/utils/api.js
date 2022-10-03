729ab80cf9cf397f71235a612a3329631ec6ebbf
Pedro Nepomuceno <pedrohnlima@gmail.com>
2022-10-02 18:25:27 -0700

    fix path

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -71,7 +71,7 @@ class Api {
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.pedronepomuceno.students.nomoredomainssbs.ru"
+    ? "http://api.pedronepomuceno.students.nomoredomainssbs.ru"
     : "http://localhost:3000";
 
 const api = new Api({

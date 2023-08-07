8057c9986028b35b882b1067fe22f7f6ce24072e
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-06 17:29:15 -0700

    update eslintrc

--- frontend/src/components/ImagePopup.js
+++ frontend/src/components/ImagePopup.js
@@ -1,22 +1,24 @@
 import React from "react";
+
 function ImagePopup({ card, onClose }) {
-	return (
-		<div className={`popup ${card ? "popup_open" : ""}`} id="photo">
-			<div className="popup__content popup__content_type_image">
-				<button
-					className="popup__close"
-					type="button"
-					onClick={onClose}></button>
+  return (
+    <div className={`popup ${card ? "popup_open" : ""}`} id="photo">
+      <div className="popup__content popup__content_type_image">
+        <button
+          className="popup__close"
+          type="button"
+          onClick={onClose}
+        ></button>
 
-				<img
-					className="popup__image"
-					src={card ? card.link : ""}
-					alt={card ? card.name : ""}
-				/>
+        <img
+          className="popup__image"
+          src={card ? card.link : ""}
+          alt={card ? card.name : ""}
+        />
 
-				<p className="popup__caption">{card ? card.name : ""}</p>
-			</div>
-		</div>
-	);
+        <p className="popup__caption">{card ? card.name : ""}</p>
+      </div>
+    </div>
+  );
 }
 export default ImagePopup;

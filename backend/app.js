485e4636e6df78765889d7dfc8f248ddfd4bee43
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-29 12:40:55 -0700

    update

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,19 @@ const limiter = require("./middleware/rateLimiter");
 
 app.use(limiter);
 
+mongoose.set("strictQuery", false);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 // mongoose.connect("mongodb://localhost:27017/react-around-api-full");
 
-mongoose.connect("mongodb://127.0.0.1:27017/react-around-api-full");
+// this mongoose.connect("mongodb://127.0.0.1:27017/react-around-api-full");
 
 const { requestLogger, errorLogger } = require("./middleware/logger");
 

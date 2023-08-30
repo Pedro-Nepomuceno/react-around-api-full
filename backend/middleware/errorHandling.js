59a41f6868876b87a8c95a523526e22dc7b34445
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-29 18:22:20 -0700

    update

--- backend/middleware/errorHandling.js
+++ backend/middleware/errorHandling.js
@@ -3,6 +3,7 @@ module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     // check the status and display a message based on it
-    message: statusCode === 500 ? "An error occurred on the server" : message,
+    message:
+      statusCode === 500 ? "An error occurred on the server yoo" : message,
   });
 };

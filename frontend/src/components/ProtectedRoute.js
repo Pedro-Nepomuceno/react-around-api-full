f851f6eda81ae3a1b8df13782d0073363e313ed8
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-30 17:55:57 -0700

    update

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 function ProtectedRoute({ children, loggedIn, ...props }) {
-	return (
-		<Route {...props}>
-			{loggedIn ? children : <Redirect to={"/signin"} />}
-		</Route>
-	);
+  return (
+    <Route {...props}>
+      {loggedIn ? children : <Redirect to={"/react-around-api-full/signin"} />}
+    </Route>
+  );
 }
 
 export default ProtectedRoute;

be252bc54284869d1ea08bb156d2249eb9c9b2ab
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-04-04 12:22:53 -0700

    update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -79,11 +79,9 @@ class Api {
 //     : "http://localhost:3000";
 
 const api = new Api({
-  baseUrl: "http://54.89.29.89:3001",
+  baseUrl: "https://mysubdomain.apiaroundreact.net",
   headers: {
     "Content-Type": "application/json",
   },
 });
 export default api;
-
-// https://different-cowboy-hat-fly.cyclic.cloud

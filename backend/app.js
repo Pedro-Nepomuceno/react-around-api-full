6f3f8764620dd9af238c78e72662d55933fcbb98
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-04-25 16:45:06 -0700

    update cors

--- backend/app.js
+++ backend/app.js
@@ -38,10 +38,10 @@ app.use((req, res, next) => {
     "https://react-around-api-full-rho.vercel.app"
   );
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  // );
   next();
 });
 

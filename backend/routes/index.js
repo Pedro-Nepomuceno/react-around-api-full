2a8936f8d76eef82f67023a899bcd893a4dff165
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-05-03 10:40:02 -0700

    refac middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 const { createUser, login } = require("../controllers/users");
 
 const userRouter = require("./users");
@@ -10,7 +10,7 @@ const { validateLogin, validateUser } = require("../middleware/validation");
 router.post("/signup", validateUser, createUser);
 router.post("/signin", validateLogin, login);
 
-router.use(auth);
+// router.use(auth);
 
 router.use("/users", userRouter);
 router.use("/cards", cardRouter);
